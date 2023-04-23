f1e86a531a68672f26eca58ce173e1ab4e05c408
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-23 20:33:10 +0000

    Updated

--- utils.c
+++ utils.c
@@ -19,4 +19,11 @@ return (0);
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
+char map_to[] = "0123456789ABCDEF";
+/* The hexa format code is always 2 digits long */
+if (ascii_code < 0)
+ascii_code *= -1;
+buffer[i++] = '\\';
+buffer[i++] = 'x';
+
 
