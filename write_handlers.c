bcfff466077b819b89a8cb4d6a21a8c2109daee1
bartazouhair <bartazouhair@gmail.com>
2023-04-23 14:05:05 -0700

    Updated

--- write_handlers.c
+++ write_handlers.c
@@ -247,7 +247,7 @@ int write_pointer(char buffer[], int ind, int length,
 	buffer[--ind] = 'x';
 	buffer[--ind] = '0';
 	if (extra_c)
-		buuffer[--ind] = extra_c;
+		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
 
