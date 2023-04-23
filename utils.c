b90dc72e3168fc082a28cd12dae077a5e2f592a9
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-23 20:34:50 +0000

    Updated

--- utils.c
+++ utils.c
@@ -25,5 +25,11 @@ if (ascii_code < 0)
 ascii_code *= -1;
 buffer[i++] = '\\';
 buffer[i++] = 'x';
+buffer[i++] = map_to[ascii_code / 16];
+buffer[i] = map_to[ascii_code % 16];
+return (3);
+}
+
+
 
 
