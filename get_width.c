5c62b17dc400114b8d53d015e966154a3c964501
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-24 13:35:12 +0000

    Updated

--- get_width.c
+++ get_width.c
@@ -10,6 +10,9 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
+	int curr_i;
+	int width = 0;
+
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -21,4 +24,14 @@ int get_width(const char *format, int *i, va_list list)
 		{
 			curr_i++;
 			width = va_arg(list, int);
-			break;²	}
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (width);
+}
+
