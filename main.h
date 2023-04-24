71ed20b17878df8998b804d07758060f6877d997
Elwahbi1988 <amineelwahbi@outlook.fr>
2023-04-24 13:45:54 +0000

    updated

--- main.h
+++ main.h
@@ -37,7 +37,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
-
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
